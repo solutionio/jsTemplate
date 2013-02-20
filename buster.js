8030a399d0f7fa1ede4dd6621b3562932cfb4881
mcp <Admin@clu.mytaxi.com>
2013-02-20 16:25:52 +0100

    Removing useless jquery dep

--- buster.js
+++ buster.js
@@ -3,7 +3,6 @@ var config = exports;
 config['Browser'] = {
   environment: 'browser',
   sources: [
-    'components/jquery/jquery.js', 
     'components/async-load/load.js', 
     'JSTemplate.js',
     'JSTemplate.load.js'
