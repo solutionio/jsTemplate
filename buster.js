cbb21459e60b8ca29052f1872e7e4a0a833c1c2b
mcp <Admin@clu.mytaxi.com>
2013-02-20 13:28:30 +0100

    Loading the loader

--- buster.js
+++ buster.js
@@ -4,6 +4,7 @@ config['Browser'] = {
   environment: 'browser',
   sources: [
     'components/jquery/jquery.js', 
+    'components/async-load/load.js', 
     'JSTemplate.js',
     'JSTemplate.load.js'
   ],
